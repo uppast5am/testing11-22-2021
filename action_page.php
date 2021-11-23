b64b61f4b7289bb3da92e74f88c7567296ba71a4
uppast5am <92594564+uppast5am@users.noreply.github.com>
2021-11-22 17:31:01 -0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,14 +1,11 @@
 <?php
 // define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
+$name = $password "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
-}
+  $name = test_input($_POST["uname"]);
+  $password = test_input($_POST["psw"]);
+
 
 function test_input($data) {
   $data = trim($data);
